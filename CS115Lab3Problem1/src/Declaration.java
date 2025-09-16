ba93416e61271a9090e400126883332074e8a548
mablakley <mablakley@cps.edu>
2025-09-16 10:16:06 -0500

    Problem2

---
+++ CS115Lab3Problem1/src/Declaration.java
@@ -0,0 +1,27 @@
+public class Declaration {
+	public static void main(String[] args) {
+
+// Enter your variable declaration statements below that line
+		String IITZipCode;
+		String sampleUKPostalCode;
+		int myAge;
+		char myGrade;
+		long earthMoonDistanceInYards;
+
+// Enter your variable initialization statements below that line
+		IITZipCode = "66066";
+		sampleUKPostalCode = "SW1A 1AA";
+		myAge = 17;
+		myGrade = 'A';
+		earthMoonDistanceInYards = 413600000;
+		
+
+// Display it
+		System.out.println("IIT Zip Code is: " + IITZipCode);
+		System.out.println("Sample UK postal code is: " + sampleUKPostalCode);
+		System.out.println("My age is: " + myAge);
+		System.out.println("Desired CS115 grade: " + myGrade);
+		System.out.println("Earth-Moon distance in yards: " + earthMoonDistanceInYards + " yards");
+
+	}
+}
